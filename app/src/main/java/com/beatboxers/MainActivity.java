bcb568cd9941f9340490a598a58059631c60e0bd
Vadim Zapolski <v.zapolskydovnar@gmail.com>
2016-04-09 16:13:59 +0300

    settings menu item added

--- app/src/main/java/com/beatboxers/MainActivity.java
+++ app/src/main/java/com/beatboxers/MainActivity.java
@@ -24,12 +24,14 @@ import android.view.ViewConfiguration;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.beatboxers.activities.SettingsActivity;
 import com.beatboxers.bluetooth.ConnectionCallback;
 import com.beatboxers.bluetooth.ConnectionService;
 import com.beatboxers.bluetooth.device.Rfduino;
 import com.beatboxers.bluetooth.device.SavedDevices;
 import com.beatboxers.dialogs.AboutUsDialog;
 import com.beatboxers.dialogs.ScanDialog;
+import com.beatboxers.fragments.SettingsFragment;
 import com.beatboxers.fragments.FragmentDevice;
 import com.beatboxers.fragments.FragmentDeviceHeader;
 import com.beatboxers.fragments.FragmentPants;
@@ -49,6 +51,7 @@ public class MainActivity extends Activity {
     private DeviceConfig mDeviceConfig;
     private ScanDialog mScanDialog = new ScanDialog();
     private AboutUsDialog mAboutUsDialog = new AboutUsDialog();
+    private SettingsFragment mSettingsFragment = new SettingsFragment();
 
     private final BroadcastReceiver mBluetoothBroadcastReceiver = new BroadcastReceiver() {
         @Override
@@ -287,7 +290,8 @@ public class MainActivity extends Activity {
             case R.id.menuAbout:
                 mAboutUsDialog.show(getFragmentManager(), AboutUsDialog.TAG);
                 return true;
-            case R.id.menuTutorial:
+            case R.id.menuSettings:
+                startActivity(new Intent(this, SettingsActivity.class));
                 return true;
         }
 
